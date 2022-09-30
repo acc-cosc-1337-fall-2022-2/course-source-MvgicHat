cd43afa707befb7a366b35577af087ca4966ac79
MvgicHat <aaron.bond2@g.austincc.edu>
2022-09-30 09:41:40 -0500

    class notes 9/28

--- test/examples/07_vector/07_vector_tests.cpp
+++ test/examples/07_vector/07_vector_tests.cpp
@@ -1,6 +1,12 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "function_vec.cpp"
+
+using std::vector;
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
+
+
+TEST_CASE("Class example");
