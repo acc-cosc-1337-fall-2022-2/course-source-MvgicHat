edcaf9daf16a8e233b72ae50b7156c91fed7fdd7
MvgicHat <aaron.bond2@g.austincc.edu>
2022-10-01 17:15:49 -0500

    homework 4 complete

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,21 +1,43 @@
-//write include statements
-
-#include<iostream>
-
-//write using statements
+#include <iostream>
+#include "dna.h"
 
 using std::cin;
 using std::cout;
+using namespace std;
 
-
-/*
-Write code that prompts user to enter 1 for Get GC Content, 
-or 2 for Get DNA Complement.  The program will prompt user for a 
-DNA string and call either get gc content or get dna complement
-function and display the result. Program runs as long as 
-user enters a y or Y.
-*/
-int main() 
+int main()
 {
-	return 0;
+	int menu_selection; int num; int usernum1; int usernum2; int factor_answer; int gcd_answer; std::string exi;
+	do
+	{
+		cout << "\n1-Factorial\n2-Great Common Divisor\n3-Exit\n";
+		cin >> menu_selection;
+
+		if (menu_selection == 1)
+		{
+			cout << "What number would you like to factorialize? ";
+			cin >> num;
+			factor_answer = factorial(num);
+			cout << "That number factorialized is: " << factor_answer;
+		}
+
+		if (menu_selection == 2)
+		{
+			cout << "Find the GCD! First number?\n";
+			cin >> usernum1;
+			cout << "Second number?\n";
+			cin >> usernum2;
+			gcd_answer = gcd(usernum1, usernum2);
+			cout << "The Greatest Common Divisor of those two numbers is " << gcd_answer << ".";
+		}
+
+		else if (menu_selection == 3)
+		{
+			cout << "Are you sure you wish to exit? (Y/N)\n";
+			cin >> exi;
+		}
+
+	} while (menu_selection == 1 || menu_selection == 2);
+
 }
+
