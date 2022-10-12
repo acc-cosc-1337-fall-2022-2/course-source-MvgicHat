d36a89a850596f0474f1b8e898d44d0e0cba9837
MvgicHat <aaron.bond2@g.austincc.edu>
2022-10-12 18:09:22 -0500

    notes 10/12 part 1

--- src/examples/11_memory/01_ref_pointers/main.cpp
+++ src/examples/11_memory/01_ref_pointers/main.cpp
@@ -1,8 +1,21 @@
 #include "ref_pointers.h"
 #include<iostream>
 
+using std::cout;
+
 int main() 
 {
-	
+	int num = 20;
+	int* num_ptr = &num;
+
+	cout<<"Value of num via num: "<<num<<"\n";
+	cout<<"Value of num via num_ptr: "<<*num_ptr<<"\n";
+	cout<<"Address of num: "<<&num<<"\n";
+
+	*num_ptr = 30;
+
+	cout<<"Value of num via num: "<<num<<"\n";
+	cout<<"Value of num via num_ptr: "<<*num_ptr<<"\n";
+
 	return 0;
 }
