c6660c25ece5a9d5411d1c523ca5bb71e8dc3641
MvgicHat <aaron.bond2@g.austincc.edu>
2022-10-12 20:05:38 -0500

    notes 10/12 part 2

--- src/examples/11_memory/02_dynamic_memory/main.cpp
+++ src/examples/11_memory/02_dynamic_memory/main.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
+using std::cout;
+
 int main() 
 {
-	
+	int* num_ptr = new int(5);//create dynamic memory
+	//use memory
+	cout<<*num_ptr<<"\n";
+	//delete memory
+	delete num_ptr;
+	num_ptr = nullptr;
+
 	return 0;
 }
