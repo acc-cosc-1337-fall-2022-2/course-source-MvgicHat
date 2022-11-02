e660e9465b1acf06df7840a181b83a793c8eaa68
MvgicHat <aaron.bond2@g.austincc.edu>
2022-11-02 17:29:26 -0500

    updating main with homework 6

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,29 @@
+//main
+#include "tic_tac_toe.h"
+
 
 int main() 
 {
-
+	TicTacToe game;
+	string first_player;
+	char end = 'N';
+	int position;
+	cout << "Tic-Tac-Toe Time!\n";
+	cout << "Moves you can make:\n1|2|3\n4|5|6\n7|8|9\n";
+	do {
+		cout<<"Player 1 choose X or O: ";
+		cin>>first_player;
+		game.start_game(first_player);
+		while (!game.game_over()) {
+			game.display_board();
+			cout << "Player "<<game.get_player()<<", select position 1-9: ";
+			cin >> position;
+			game.mark_board(position);
+		}
+		game.display_board();
+		cout<<"\nExit program? (Y/N): ";
+		cin>>end;
+	} while(end != 'Y');
 	return 0;
 }
+
