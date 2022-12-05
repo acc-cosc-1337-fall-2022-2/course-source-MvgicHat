38a97a13b351deb6f29d100626e2db2d5fe0afe7
MvgicHat <aaron.bond2@g.austincc.edu>
2022-12-04 22:09:41 -0600

    Homework 5

--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,14 +1,48 @@
-/*
-use a vector of int with values 8, 4, 20, 88, 66, 99
+#include "func.h"
 
-Prompt user for 1 for Get Max from vector and 2 for Get primes.
-Prompt user for a number and return max value or list of primes
-and display them to screen.
-Program continues until user decides to exit.
-
-*/
-int main() 
+int main()
 {
+    char repeat = 'Y';
+
+    while(repeat == 'Y' || repeat == 'y') {
+        //Menu
+        int selection = 0;
+
+        cout << "1 - Get GC Content\n"
+                "2 - Get DNA Complement\n"
+                "3 - Exit\n\n"
+                "Make a Selection: ";
+
+        cin >> selection;
+
+        if (selection == 3) //Exit
+        {
+            exit(0);
+        }
+
+        //Get user input
+        string dna;
+        cout << "\n\nPlease input a string of DNA: ";
+        cin >> dna;
+
+        for (char &i: dna) {
+            i = toupper(i);
+        }
+
+        if (selection == 1)
+        {
+            cout<<"GC-content: "<<get_gc_content(dna);
+            cout<<"\nRun the program again? (Y/N) ";
+            cin>>repeat;
+
+        } else if (selection == 2)
+        {
+            cout<<"The compliment is: "<<get_dna_complement(dna);
+            cout<<"\nRun the program again? (Y/N) ";
+            cin>>repeat;
+        }
+
+    }
 
 	return 0;
 }
